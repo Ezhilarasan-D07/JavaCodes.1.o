60e6a3bae2b5afce4057a9f56a272f7bbf2aebdf
ezhild07 <ezhilarasand07@gmail.com>
2025-08-12 11:45:27 +0530

    main - hello world

--- basicprograms/Main.java
+++ basicprograms/Main.java
@@ -1,20 +1,5 @@
 public class Main {
-
-   static void fibonacciSeries(int num) {
-      int numOne = 0, numTwo = 1;
-      System.out.print(numOne + " " + numTwo);
-      for (int i = 2; i < num; i += 1)  {
-         int sum;
-         sum = numOne + numTwo;
-         System.out.print(" " +sum);
-         numOne = numTwo;
-         numTwo = sum;
-      }
-   }
-
-   public static void main(String[] args) {
-      int number = 20;
-      System.out.println("Fibonacci Series :");
-      fibonacciSeries(number);
+  public static void main(String[] args) {
+      System.out.print("Hello World");
    }
 }
