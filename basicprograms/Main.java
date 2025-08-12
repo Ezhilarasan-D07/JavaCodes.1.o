21cfca9090da1c47b47ab2c490cbc93f62a1fa93
ezhild07 <ezhilarasand07@gmail.com>
2025-08-12 10:59:47 +0530

    fibonacci series corrected

--- basicprograms/Main.java
+++ basicprograms/Main.java
@@ -1,7 +1,20 @@
-
-
 public class Main {
-   public static void main(String ... args) {
-      System.out.print("Welcome Git.");
+    
+   static void printFibonacci(int n) {
+      int a = 0, b = 1;
+      System.out.print(a + " " + b);
+
+      for (int i = 2; i < n; i += 1) {
+         int next = a + b;
+         System.out.print(" " + next);
+         a = b;
+         b = next;
+      }
    }
+
+    public static void main(String[] args) {
+        int n = 10; 
+        System.out.print("Fibonacci Series: ");
+        printFibonacci(n);
+    }
 }
