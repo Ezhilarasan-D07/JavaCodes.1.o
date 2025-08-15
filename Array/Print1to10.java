035b43e074c3d54ed377d5de4dde2836bf176c1b
ezhild07 <ezhilarasand07@gmail.com>
2025-08-15 22:56:52 +0530

    array problems

---
+++ Array/Print1to10.java
@@ -0,0 +1,11 @@
+public class Print1to10 {
+   static void print1to10 () {
+      for (int ind = 1; ind <= 10; ind += 1) {
+         System.out.print(ind + " ");
+      }
+   }
+
+   public static void main(String[] args) {
+      print1to10();
+   }
+}
