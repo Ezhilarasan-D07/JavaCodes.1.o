8f92214e0d835c594b1c8eb1b7060918cb9b83d2
ezhild07 <ezhilarasand07@gmail.com>
2025-08-18 21:35:24 +0530

    Rotate by k

---
+++ Array/RotateByK.java
@@ -0,0 +1,23 @@
+public class RotateByK {
+   static void reverse (int arr [], int a, int b) {
+      arr[a] = arr[b] + arr[a] - (arr[b] = arr[a]);
+   }
+
+   static void rotateArrayByK (int arr [], int k) {
+      int len = arr.length;
+      k = k % len;
+      reverse(arr, 0, len - k - 1);
+      reverse(arr, len - k, len - 1);
+      reverse(arr, 0, len - 1);
+   }
+
+   public static void main(String ... args) {
+      int array [] = {1, 2, 3, 4, 5, 6, 7};
+      int k = 5;
+      rotateArrayByK(array, k);
+      for (int num: array) {
+         System.out.print(num + " ");
+      }
+ 
+   }
+}
