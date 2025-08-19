a7375ecf6c47e61f1fac4bdcf790bb0c4977433e
ezhild07 <ezhilarasand07@gmail.com>
2025-08-19 12:47:19 +0530

    modified repeating number

--- Array/RepeatingNumber.java
+++ Array/RepeatingNumber.java
@@ -3,7 +3,7 @@ public class RepeatingNumber {
       int arr [] = {1, 2, 2, 3, 3, 3, 3, 4, 5, 5, 5, 6};
       System.out.print("repeating numbers : ");
       for (int ind = 0; ind < arr.length; ind += 1) {
-         for (int i = ind + 1; i < arr.length; i += 1) {
+         for (int i = ind + 1; i < arr.length;) {
             if (arr[ind] == arr[i])
                System.out.print(arr[ind] + " ");
                break;
