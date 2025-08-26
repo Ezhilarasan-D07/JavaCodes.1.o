0cf1240bef6d5333ffff2432ef7a806fb1488df1
Ezhilarasan-D07 <ezhilarasand07@gmail.com>
2025-08-26 18:28:46 +0530

    modified reverse in groups.

--- Array/RotateByK.java
+++ Array/RotateByK.java
@@ -1,23 +1,26 @@
 public class RotateByK {
-   static void reverse (int arr [], int a, int b) {
-      arr[a] = arr[b] + arr[a] - (arr[b] = arr[a]);
+   static void reverse(int arr[], int a, int b) {
+      while (a < b) {
+         arr[a] = arr[b] + arr[a] - (arr[b] = arr[a]);
+         a++;
+         b--;
+      }
    }
 
-   static void rotateArrayByK (int arr [], int k) {
+   static void rotateArrayByK(int arr[], int k) {
       int len = arr.length;
-      k = k % len;
+      k = k % len;  
       reverse(arr, 0, len - k - 1);
       reverse(arr, len - k, len - 1);
       reverse(arr, 0, len - 1);
    }
 
-   public static void main(String ... args) {
+   public static void main(String... args) {
       int array [] = {1, 2, 3, 4, 5, 6, 7};
-      int k = 5;
+      int k = 10;
       rotateArrayByK(array, k);
-      for (int num: array) {
+      for (int num : array) {
          System.out.print(num + " ");
       }
- 
    }
 }
