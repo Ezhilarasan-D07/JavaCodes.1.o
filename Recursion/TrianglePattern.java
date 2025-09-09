343c9398316b3e6925641f7f2e8e060a8b281f72
Ezhilarasan-D07 <ezhilarasand07@gmail.com>
2025-09-09 20:26:15 +0530

    print triangle star pattern

---
+++ Recursion/TrianglePattern.java
@@ -0,0 +1,16 @@
+public class TrianglePattern {
+    static void triangle (int row, int col) {
+        if (row == 0)
+            return;
+        if (col < row)  {
+            System.out.print("* ");
+            triangle(row, col + 1);
+        }   else {
+            System.out.println();
+            triangle(row - 1, 0);
+        }
+    }
+    public static void main (String ... args) {
+        triangle(4, 0);
+    }
+}
