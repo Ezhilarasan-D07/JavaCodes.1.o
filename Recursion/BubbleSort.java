045369c353541da553e86b419f1925916ec5ebbb
Ezhilarasan-D07 <ezhilarasand07@gmail.com>
2025-09-10 16:55:17 +0530

    print bubble sort

---
+++ Recursion/BubbleSort.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+public class BubbleSort {
+    static void bubbleSort (int arr [], int row, int col) {
+        if (row == 0)
+            return;
+        if (col < row) {
+            if (arr[col] > arr[col + 1]) {
+                arr[col] = arr[col + 1] + arr[col] - (arr[col + 1] = arr[col]);
+            }   bubbleSort(arr, row, col + 1);
+        }   else {
+            bubbleSort(arr, row - 1 , 0);
+        }
+    }
+
+    public static void main(String ... args) {
+        int array [] = {22, 16, 6, 7, 26, 10, 15};
+        bubbleSort(array, array.length - 1, 0);
+        System.out.println(Arrays.toString(array));
+    }
+}
